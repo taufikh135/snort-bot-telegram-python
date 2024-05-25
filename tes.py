4c32e44c860566eeaba32d7c3440decc4433f7bf
Taufik Hidayat <opik.1320@gmail.com>
2024-05-25 11:31:04 +0700

    d

--- tes.py
+++
@@ -1,6 +0,0 @@
-import re
-
-text = '05/25-07:52:55.923404  [**] [1:1228:7] SCAN nmap XMAS [**] [Classification: Attempted Information Leak] [Priority: 2] {TCP} 192.168.5.100:47222 -> 192.168.5.98:23'
-regex = r'([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})'
-result = re.findall(regex, text)
-print(result)
