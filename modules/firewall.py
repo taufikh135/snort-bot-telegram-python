db32a9a9bc38045589027d2e4364a87cb1247e7e
Taufik Hidayat <opik.1320@gmail.com>
2024-05-25 11:30:39 +0700

    bugfix firewall

--- modules/firewall.py
+++ modules/firewall.py
@@ -1,14 +1,20 @@
 import subprocess
 
 class Firewall:    
+    __blockedIP = []
+    
     def __command(self, command: str) -> str:
         return subprocess.check_output(command, shell=True, text=True)
     
-    def blockIP(self, ip: str) -> str:        
-        return self.__command(f"sudo iptables -I INPUT -s {ip} -j DROP")
+    def blockIP(self, ip: str) -> None:  
+        if ip in self.__blockedIP: 
+            return
+              
+        self.__command(f"sudo iptables -I INPUT -s {ip} -j DROP")
+        self.__blockedIP.append(ip)
     
-    def removeBlockIP(self, ip: str) -> str:
-        return self.__command(f"sudo iptables -D INPUT -s {ip} -j DROP")
+    def removeBlockIP(self, ip: str) -> None:
+        self.__command(f"sudo iptables -D INPUT -s {ip} -j DROP")
         
     def allowIP(self, ip: str) -> None:
         return self.__command(f"sudo iptables -I INPUT -s {ip} -j ACCEPT")
